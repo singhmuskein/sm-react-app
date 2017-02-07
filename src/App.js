90a92fc81772f31d7d7433532bae7a6715f3fabd
Muskein Singh <muskein.singh@Muskeins-MacBook-Pro.local>
2017-02-08 01:03:08 +0530

    Basic UI Ready

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import profilePicture from './ProfilePic.JPG';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="container">
+        <div className="white-patch">
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <div className="green-patch">
+              <div className="description">
+                  <h2 className="nameText"> Hi, I'm Muskein.</h2>
+                  <h3 className="workText"> I work for the React Native team at Flipkart, Bangalore.</h3>
+              </div>
+          </div>
+          <div className="circle">
+            <img src={profilePicture} className="profilePicture"/>
+          </div>
       </div>
     );
   }
